a427182fc2daffad6ee964ce214cb0456b33f0f2
yiinuma <y.iinuma211@gmail.com>
2022-01-16 16:59:21 +0900

    init

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,6 @@
-/* eslint-disable global-require */
 module.exports = {
   style: {
     postcssOptions: {
-      // eslint-disable-next-line import/no-extraneous-dependencies
-      // eslint-disable-next-line global-require
-      // eslint-disable-next-line import/no-extraneous-dependencies
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
