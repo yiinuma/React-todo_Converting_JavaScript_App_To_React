55bc23165fed35dbe56507169935f7cb4b16f0e4
yiinuma <y.iinuma211@gmail.com>
2022-01-16 18:14:58 +0900

    first

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,29 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'react/button-has-type': [
+      'error',
+      {
+        button: ['boolean'],
+        submit: ['boolean'],
+        reset: ['boolean'],
+      },
+    ],
   },
 };
